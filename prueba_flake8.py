2c47b57b4d09252bab958b21583d72ac2f30d14a
Cesar <cesarguello18@gmail.com>
2021-08-12 23:08:26 -0600

    Corregidos los errores de formato

--- prueba_flake8.py
+++ prueba_flake8.py
@@ -1,12 +1,9 @@
-import math  # Se importa la libreria de operaciones matematicas
-
-
 # Descripcion: funcion que realiza la suma de dos números
 # Entrada: dos números
 # Salida: la suma de ambos números
-def sumar(a,b):
+def sumar(a, b):
     c = a + b  # almacena el resultado de la suma en una variable
-    return a + b  # retorna el resultado de la suma
+    return c  # retorna el resultado de la suma
 
 
 print(sumar(2, 5))  # llama a la función sumar e imprime el resultado
